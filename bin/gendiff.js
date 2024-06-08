71725fbfc023c678cdeb998a755600f058de1a77
Dmitry Skladchikov <dsklvdchikov@mail.ru>
2024-06-08 14:33:27 +0300

    Completed the second step of the project

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,10 +3,13 @@ import { program } from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'output usage information');
 
-  program.parse();
+program.parse();
 
 
 
