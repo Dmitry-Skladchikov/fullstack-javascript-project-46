932c762db9a4ad73d8dae67905b9b90ae9e8741b
Dmitry Skladchikov <dsklvdchikov@mail.ru>
2024-06-08 13:38:02 +0300

    Completed the first step of the project

---
+++ bin/gendiff.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information');
+
+  program.parse();
+
+
+
+
+//   import { program } from 'commander';
+
+// const join = (first, second, connector = '') => `${first}${connector}${second}`;
+
+// program
+//   .name('string-util')
+//   .description('Инструмент для работы со строками')
+//   .version('1.0.0');
+
+// program.command('join')
+//   .description('Команда соединяет две строки в одну')
+//   .argument('<first>', 'первая строка')
+//   .argument('<second>', 'вторая строка')
+//   .option('-c, --connector <type>', 'соединительная строка', '')
+//   .action((first, second, options) => {
+//     // BEGIN
+//     const result = join(first, second, options.connector);
+//     console.log(result);
+//     // END
+//   });
+
+// program.parse();
