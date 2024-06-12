3611da11a72ab6c1eda2e2ef4b2f8490efe78d12
Dmitry Skladchikov <dsklvdchikov@mail.ru>
2024-06-12 17:16:33 +0300

    completed the third step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,38 +1,15 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import genDif from '../src/index.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
+  .arguments('<filepath1> <filepath2>')
   .option('-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .action((filepath1, filepath2) => {
+    console.log(genDif(filepath1, filepath2));
+  });
 
 program.parse();
-
-
-
-
-//   import { program } from 'commander';
-
-// const join = (first, second, connector = '') => `${first}${connector}${second}`;
-
-// program
-//   .name('string-util')
-//   .description('Инструмент для работы со строками')
-//   .version('1.0.0');
-
-// program.command('join')
-//   .description('Команда соединяет две строки в одну')
-//   .argument('<first>', 'первая строка')
-//   .argument('<second>', 'вторая строка')
-//   .option('-c, --connector <type>', 'соединительная строка', '')
-//   .action((first, second, options) => {
-//     // BEGIN
-//     const result = join(first, second, options.connector);
-//     console.log(result);
-//     // END
-//   });
-
-// program.parse();
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+import process from 'node:process';
+
+// const filepath1 = '/home/dmitryskl/Документы/fullstack-javascript-project-46/test_files/file1.json';
+// const filepath2 = '/home/dmitryskl/Документы/fullstack-javascript-project-46/test_files/file2.json';
+
+const currentDir = process.cwd();
+
+const getData = path => fs.readFileSync(path, 'utf8');
+
+const genDif = (filepath1, filepath2) => {
+    const relativePath1 = path.resolve(currentDir, filepath1);
+    const relativePath2 = path.resolve(currentDir, filepath2);
+    
+    const data1 = getData(relativePath1);
+    const data2 = getData(relativePath2);
+
+    return [JSON.parse(data1), JSON.parse(data2)];
+}
+
+export default genDif;
